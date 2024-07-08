e029bcbd6126972128d0102ffa92b6d7ee8c0ceb
Momilijaz96 <momilijaz@gmail.com>
2024-07-08 22:50:24 +0500

    added mongo db ingestion logic

--- ingestion.py
+++ ingestion.py
@@ -1,22 +1,47 @@
 # Script for reading images, compressing them and storing in a MongoDB database, for later usage
 import pandas as pd
 from db import images_db
+from utils import df_preprocess, compress_image
+import cv2
 
-
-def ingest_image():
+def ingest_image(df):
     """
     Function to convert an image from df chunk to a compressed image and store it in MongoDB.
     """
-    pass
+    # Group the df by image_id
+    grouped = df.groupby('image_id')
+    
+    count = 0
+    # Extract all rows for each image_id
+    for image_id, group in grouped:
+        
+        # Get all columns except image_id and frame_num
+        image = group.drop(columns=['image_id','frame_num'])
+        
+        # Convert the df chunk to numpy array
+        image_array = image.to_numpy()
+        
+        # Compress the image
+        compressed_image = compress_image(image_array, image_array.shape[0], 150)
+
+        # Insert the compressed image to MongoDB
+        images_db.insert_image(compressed_image, image_id)
+
+        count += 1
+    
+    return count
+
 
 def main():
     # Read the CSV file
     df = pd.read_csv('img.csv',header=0)
 
-    # Convert the first column to str
-    df['depth'] = df['depth'].astype(str)
+    # preprocess the dataframe
+    df = df_preprocess(df)
 
-    # Split the depth column into frame_num and image_id
-    
+    # Ingest the images
+    count = ingest_image(df)
+    print(f"Total images ingested: {count}")
+
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -19,3 +19,15 @@ def compress_image(image,new_height,new_width):
     Compress an numpy array image to a new size
     """
     return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+def df_preprocess(df):
+    # Convert the first column to str
+    df['depth'] = df['depth'].astype(str)
+
+    # Split the depth column into frame_num and image_id, based on .
+    df[['image_id','frame_num']] = df['depth'].str.split('.',expand=True)
+
+    # Drop the depth column
+    df.drop(columns=['depth'],inplace=True)
+
+    return df
